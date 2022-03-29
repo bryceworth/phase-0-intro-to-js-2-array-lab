7accc021ae97acc72b98016290ec46fe61c712e1
bryceworth <bryceaworth@gmail.com>
2022-03-29 11:52:54 -0700

    commit tree 46dc84c31f0793cadf7438800635d0e852e7e0ae

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+
+
+let cats =  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    let newCats = cats.push(name)
+}
+function destructivelyPrependCat(name){
+    let newCats = cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    let newCats = cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    let newCats = cats.shift(name)
+}
+function appendCat(name){
+    const newCats = [...cats, name]
+    return newCats
+}
+function prependCat(name){
+    let newCats = [name, ...cats]
+    return newCats
+    
+}
+function removeLastCat(){
+    const newCats = cats.slice(0, cats.length-1)
+    return newCats
+}
+function removeFirstCat(){
+    let newCats = cats.slice(1)
+    return newCats
+}
